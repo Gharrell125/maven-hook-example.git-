c1db69ef2e3950b919a8d8e0a05e9d1c03183de4
brentreeves <brent@twinbear.com>
2019-11-22 13:16:42 -0600

    Add files via upload

---
+++ src/main/java/com/bnr/Calculator.java
@@ -0,0 +1,21 @@
+package com.bnr;
+
+/**
+ * Hello Calculator!
+ *
+ */
+public class Calculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Calculator!" );
+    }
+
+    public int add (int x, int y) {
+	return x + y;
+    }
+
+    public int subtract (int x, int y) {
+	return x - y;
+    }
+}
---
+++ src/test/java/com/bnr/CalculatorTest.java
@@ -0,0 +1,58 @@
+package com.bnr;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Calculator.
+ */
+public class CalculatorTest 
+    extends TestCase
+{
+    Calculator calc = new Calculator();
+    
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CalculatorTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CalculatorTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCalculator()
+    {
+        assertTrue( true );
+    }
+
+    public void testAdd()
+    {
+	assertEquals(4, calc.add(2,2));
+    }
+
+    public void testSubtract()
+    {
+	assertEquals(4, calc.subtract(6,2));
+	assertEquals(0, calc.subtract(2,2));
+	assertEquals(-4, calc.subtract(0,4));
+    }
+
+    // public void testSubtract2()
+    // {
+    // 	assertEquals(4, calc.subtract(6,1));
+    // }
+
+}
